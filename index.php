170b857ee988e238270f2ed72a921869add4b719
hpascua <43760125+hpascua@users.noreply.github.com>
2020-08-26 14:05:09 +0800

    update

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Hale Pascua</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Custom CSS -->
+        <link href="/css/main.css" rel="stylesheet" type="text/css" />
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    </head>
+    <body>
+        <header>
+            <?php include "header.html"?>
+        </header>
+    </body>
+</html>
